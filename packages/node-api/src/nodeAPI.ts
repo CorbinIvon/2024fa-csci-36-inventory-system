81fe36ca881d4007b192172a9794afd682d0d6d3
Corbin Meier <corbinivon@gmail.com>
2024-12-15 22:21:00 -0800

    Update dotenv configuration to specify custom path for environment variables

--- packages/node-api/src/nodeAPI.ts
+++ packages/node-api/src/nodeAPI.ts
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config({ path: '../../.env' })
 import { NodePoint, NodePointData } from './nodePoint'
 
 const GRAPHQL_PORT = process.env.GRAPHQL_PORT || 4000
